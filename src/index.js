7fb94243147dd019319ff736a836ca354588383b
VictorN326 <21vnguyen3@gmail.com>
2022-12-10 00:48:04 -0800

    Started implementing Movie API with Redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import  ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import { createTheme, ThemeProvider} from '@mui/material/styles';
+import store from './app/store';
 const theme = createTheme({});
 //Pulling the code from github on a laptop for testing.
 ReactDOM.render(
-<ThemeProvider theme = {theme}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-</ThemeProvider>,
+<Provider store = {store}>
+    <ThemeProvider theme = {theme}>
+        <BrowserRouter>
+        <App />
+        </BrowserRouter>
+    </ThemeProvider>,
+</Provider>,    
 document.getElementById('root'),
 );
