1da46a28ecbd6756af021c696c3ae77094fa7fe1
VictorN326 <21vnguyen3@gmail.com>
2022-12-24 21:12:40 -0800

    Worked on actors, button links, and added overview

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -13,6 +13,16 @@ const MovieInfo = () => {
   const {id} = useParams();
   const {data, isFetching, error} = useGetMovieQuery(id);
   const classes = useStyles();
+  const isMovieFavorite = true;
+  const isMovieWatchlisted = false;
+
+  const addToFavorites = () => {
+
+  };
+
+  const addToWatchlist = () => {
+
+  };
   if (isFetching) {
     return (
       <Box display = "flex" justifyContent="center" alignItems="center">
@@ -51,6 +61,52 @@ const MovieInfo = () => {
               <Typography color="textPrimary" variant="subtitle1">{genre?.name}</Typography>
             </Link>
           ))}
+        </Grid>
+
+        <Typography variant='h5' gutterBottom style={{marginTop: '10px'}}>Overview</Typography>
+        <Typography style={{marginBottom: '2rem'}}>{data?.overview}</Typography>
+        <Typography variant='h5' gutterBottom>Top Cast</Typography>
+
+        <Grid item container spacing={2}>
+            {data && data.credits?.cast?.map((actor,i) => (
+              //show only actors and actresses that have profile pictures for the cast and their role
+              actor.profile_path && (
+                <Grid key={i} item xs={4} md={2} component={Link} to={`/actors/${actor.id}`} style={{textDecoration: 'none'}}> 
+                  <img className={classes.castImage} src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`} alt={actor.name}/>
+                  <Typography color="textPrimary">{actor?.name}</Typography>
+                  <Typography color="textSecondary">{actor.character.split('/')[0]}</Typography>
+                </Grid>
+              )
+            )).slice(0,12)}
+        </Grid>
+        <Grid item container style={{marginTop:'2rem'}}>
+          <div className={classes.buttonsContainer}>
+            <Grid item xs={12} sm={6} className={classes.buttonsContainer}>
+              {/* Button div that holds different buttons that are links to movies information and website */}
+              <ButtonGroup size='small' variant='outlined'>
+                <Button target="_blank" rel="noopener noreferrer" href={data?.homepage} endIcon={<Language/>}>Website</Button>
+                <Button target="_blank" rel="noopener noreferrer" href={`https://www.imdb.com/title/${data?.imdb_id}`} endIcon={<MovieIcon/>}>IMDB</Button>
+                <Button onClick={()=> {}} href="#" endIcon={<Theaters/>}>Trailer</Button>
+              </ButtonGroup>
+            </Grid>
+            <Grid item xs={12} sm={6} className={classes.buttonsContainer}>
+              {/* Button div that holds different buttons that are links to movies information and website */}
+              <ButtonGroup size='medium' variant='outlined'>
+                <Button onClick={addToFavorites} endIcon={isMovieFavorite ? <FavoriteBorderOutlined/>: <Favorite/>}>
+                  {isMovieFavorite ? 'Unfavorite': 'Favorite'}
+                </Button>
+                <Button onClick={addToWatchlist} endIcon={isMovieWatchlisted ? <Remove/>: <PlusOne/>}>
+                  {/*UNNECESSARY FOR LONG BUTTON: {isMovieWatchlisted ? 'Remove from Watchlist': 'Add to Watchlist'} */}
+                  Watchlist
+                </Button>
+                <Button endIcon={<ArrowBack/>} sx={{borderColor: 'primary.main'}}>
+                  <Typography component={Link} to="/" color="inherit" variant="subtitle2" style={{textDecoration: 'none'}}>
+                    Back
+                  </Typography>
+                </Button>
+              </ButtonGroup>
+            </Grid>
+          </div>
         </Grid>
       </Grid>
     </Grid>
--- src/components/MovieInfo/styles.js
+++ src/components/MovieInfo/styles.js
@@ -43,5 +43,21 @@ export default makeStyles((theme)=> ({
         [theme.breakpoints.down('sm')]: {
             padding: '0.5rem 1rem',
         },
+        textDecoration: 'none',
     },
+    castImage:{
+        width: '100%',
+        maxWidth: '7em',
+        height: '8em',
+        objectFit: 'cover',
+        borderRadius: '10px',
+    },
+    buttonsContainer: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%',
+        [theme.breakpoints.down('sm')]: {
+            flexDirection: 'column',
+        },
+    }
 }))
